21b4e053fe24aa9aa10b1c32cb169b99e48751ad
Dan Knutsen <dknutsen@gmail.com>
2017-03-22 21:55:17 -0500

    Contenders are not fully editable excempt for saving the quick edit

--- app/components/contender-editor.js
+++ app/components/contender-editor.js
@@ -27,29 +27,47 @@ export default Ember.Component.extend({
   },
 
   // table
+  //contenders: [],
+  //columns: ['name'],
   headers: function(){
     let headers = {};
     this.get('contenders').forEach((contender) => {
       for (var property in contender) {
         if (contender.hasOwnProperty(property)) {
-          headers[property] = property;
+          let header = property.capitalize();
+          headers[header] = property;
         }
       }
     }); 
     return headers;
-  }.property('contenders', 'contenders.[]'),
+  }.property('contenders', 'contenders.[]', 'columns', 'columns.[]'),
+/*
   contenders: [
     {apple:1, pear:2, lemon:4, e:5},
     {a:0, b:0, c:0, d:0},
     {a:"q", b:"b", c:"c", d:"d", e:"e"}
   ],
-  //
+*/
 
   actions: {
     tabChanged: function(newTab){
       this.set('inputMode', newTab.replace('Tab',''));
     },
     startQuickInput: function(){
+      let contenders = this.get('contenders');
+      let columns = this.get('columns');
+      // set CSV text
+      let csvText = columns.join(',') + '\n';
+      contenders.forEach((contender) => {
+        let line = [];
+        columns.forEach((column) => {
+          line.push(contender[column]);
+        });
+        csvText += line.join(',') + '\n';
+      });
+      this.set('csvInputText', csvText);
+      // set JSON text
+      this.set('jsonInputText', JSON.stringify(contenders, null, 2));
       this.set('quickInput', true);
     },
     saveQuickInput: function(){
@@ -58,28 +76,34 @@ export default Ember.Component.extend({
     cancelQuickInput: function(){
       this.set('quickInput', false);
     },
-    addColumn: function(){
 
+    addColumn: function(){
+      this.sendAction('addColumn');
     },
     deleteColumn: function(prop){
+      this.sendAction('deleteColumn', prop);
+    },
+
+    // column name editing
+    editColumnName: function(header, prop){
+      this.set('editingColumn', prop);
+      this.set('editingColumnValue', header);
+    },
+    doneEditingColumnName: function(header, prop){
+      let value = this.get('editingColumnValue').toLowerCase();
+      // validate
+      if(value === '' || value === 'name' || (this.get('headers')[value.capitalize()] && value !== prop)) {
+        return;
+      }
+      this.set('editingColumn', null);
+      this.sendAction('columnNameChanged', prop, value);
+    },
+  }
+});
+/*
       let contenders = this.get('contenders');
       contenders.forEach((contender)=>{
         delete contender[prop];
       });
       this.set('conteders', contenders);
-    },
-    addRow: function(){
-      let headers = this.get('headers');
-      let newRow = {};
-      for (var property in headers) {
-        if (headers.hasOwnProperty(property)) {
-          newRow[property] = "";
-        }
-      }
-      this.get('contenders').pushObject(newRow);
-    },
-    deleteRow: function(index){
-      this.get('contenders').removeAt(index);
-    }
-  }
-});
+*/
--- app/controllers/brackets/new.js
+++ app/controllers/brackets/new.js
@@ -11,6 +11,65 @@ export default Ember.Controller.extend({
   visibilityOptions: ["Private", "Shared", "Public"],
   visibility: "Private",
 
+  contenders: [],
+  columns: ['name'],
+
+  setup: function(){
+    this.send('numContendersChanged', this.get('numContenders'));
+  }.on('init'),
+
+  actions: {
+    numContendersChanged: function(value){
+      this.set('numContenders', value);
+      let contenders = this.get('contenders');
+      let columns = this.get('columns');
+      let oldLength = contenders.get('length');
+      if(oldLength > value) {
+        contenders.removeAt(value, oldLength - value);
+      } else {
+        for(var i=0; i<value-oldLength; i++) {
+          let newRow = {'name': ''};
+          for(var j=0; j<columns.length; j++){
+            newRow[columns[j]] = '';
+          }
+          contenders.pushObject(newRow);
+        }
+      }
+    },
+    addColumn: function(){
+      this.get('columns').addObject('new column');
+      this.get('contenders').forEach((contender)=>{
+        contender['new column'] = '';
+      });
+    },
+    deleteColumn: function(prop){
+      this.get('columns').removeObject(prop);
+      this.get('contenders').forEach((contender)=>{
+        delete contender[prop];
+      });
+    },
+    columnNameChanged: function(prop, newProp){
+      this.get('columns').removeObject(prop);
+      this.get('columns').addObject(newProp);
+      this.get('contenders').forEach((contender)=>{
+        contender[newProp] = contender[prop];
+        delete contender[prop];
+      });
+    },
+    submit: function(form){
+      console.log("REPLACE THIS WITH A RECORD CREATE");
+      console.log({
+        name: form.name,
+        blind: form.blind,
+        contenders: form.contenders,
+        type: form.type,
+        visibility: form.visibility
+      });
+    },
+  }
+});
+
+
 
 /*
   parsedCSV: function(){
@@ -32,16 +91,4 @@ export default Ember.Controller.extend({
   },
 */
 
-  actions: {
-    submit: function(form){
-      console.log("REPLACE THIS WITH A RECORD CREATE");
-      console.log({
-        name: form.name,
-        blind: form.blind,
-        contenders: form.contenders,
-        type: form.type,
-        visibility: form.visibility
-      });
-    },
-  }
-});
+
