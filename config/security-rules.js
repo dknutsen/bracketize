7bb9abc11cc7302ad3d969361d71fdbe450869a2
Dan Knutsen <dknutsen@gmail.com>
2017-06-05 09:20:06 -0500

    Add security rules to version control

---
+++ config/security-rules.js
@@ -0,0 +1,50 @@
+{
+  "rules": {
+    "brackets": {
+      ".read": "auth != null",
+      "$bracket":{
+        ".write": "auth != null && (newData.child('owner').val() === auth.uid || data.child('owner').val() === auth.uid)"
+        // add validation rules?
+        // add index?
+      },
+    },
+    "contenders": {
+      ".read": "auth != null",
+      "$contender":{
+        ".write": "auth != null && (newData.child('owner').val() === auth.uid || data.child('owner').val() === auth.uid)"
+        // add validation rules?
+        // add index?
+      },
+    },
+    "matches": {
+      ".read": "auth != null",
+      "$match":{
+        ".write": "auth != null"
+        // add validation rules?
+        // add index?
+      },
+    },
+    "rounds": {
+      ".read": "auth != null",
+      "$round":{
+        ".write": "auth != null && (newData.child('owner').val() === auth.uid || data.child('owner').val() === auth.uid)"
+        // add validation rules?
+        // add index?
+      },
+    },
+    "votes": {
+      ".read":"auth != null",
+      "$vote":{
+        ".write": "auth != null && (newData.child('owner').val() === auth.uid || data.child('owner').val() === auth.uid)"
+        // add validation rules?
+        // add index?
+      },
+    }
+    
+//    "users": {
+//      "$user": {
+//        ".read": "data.child('public').val() == true"
+//      }
+//    }    
+  }
+}
