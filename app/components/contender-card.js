e1e25baa2367c8bfde2ab8f535b13c75062c45f7
Dan Knutsen <dknutsen@gmail.com>
2017-08-08 13:10:19 -0500

    Make scores private until owner or match is closed

--- app/components/contender-card.js
+++ app/components/contender-card.js
@@ -5,6 +5,9 @@ export default Ember.Component.extend({
   showBlind: function(){
     return !this.get('bracketClosed') && !this.get('isOwner') && this.get('isBlind');
   }.property('bracketClosed', 'isBlind', 'isOwner'),
+  showScores: function(){
+    return this.get('matchIsClosed') || this.get('isOwner');
+  }.property('matchIsClosed', 'isOwner'),
 
   actions: {
     contenderClicked: function(contender){
